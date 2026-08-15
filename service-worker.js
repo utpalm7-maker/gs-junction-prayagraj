4af2fb4dfe6c9709a9a6f9cc783ef6d30007ca98
utpalm7-maker <utpalm7@gmail.com>
2026-08-15 10:32:23 +0530

    Create service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,11 @@
+gs-junction-prayagraj/
+│
+├── tests/
+│   ├── index.json
+│   └── tgt-gs-001.txt
+│
+├── index.html        <-- Main File
+├── style.css
+├── app.js
+├── manifest.json
+└── service-worker.js <-- (यहाँ रूट फ़ोल्डर में बनाएँ)
